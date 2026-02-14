4889dad0abddc23154d0bdd87083f45df66fdb25
Antony Chirinos <chirinosbonillaantonyariel@gmail.com>
2026-02-14 14:17:49 -0600

    Dando Detalles :v

---
+++ CreandoRepositorio/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreandoRepositorio
+{
+    public class POO
+    {
+        //Nose porque desaparecio el archivo program haha
+    }
+}
